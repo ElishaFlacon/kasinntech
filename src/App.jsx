35a9c4bc491172d390ae0ddaea1928e0473f54e9
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-09-11 21:52:39 +0500

    feat: giga update

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,39 @@ import Parallax from 'parallax-js'
 
 export const App = () => {
     useEffect(() => {
-        const scene = document.getElementById('scene');
-        new Parallax(scene);
+        const scene1 = document.getElementById('scene-1');
+        const scene2 = document.getElementById('scene-2');
+        const scene3 = document.getElementById('scene-3');
+        new Parallax(scene1);
+        new Parallax(scene2);
+        new Parallax(scene3);
     }, [])
 
     return (
         <>
-            <div id="scene">
+            <div id="scene-1">
                 <img
                     className="background"
                     data-depth="0.4"
-                    src="image/background.svg"
+                    src="image/background-3.svg"
+                    alt=""
+                />
+            </div>
+
+            <div id="scene-2">
+                <img
+                    className="background"
+                    data-depth="0.4"
+                    src="image/background-1.svg"
+                    alt=""
+                />
+            </div>
+
+            <div id="scene-3">
+                <img
+                    className="background"
+                    data-depth="0.4"
+                    src="image/background-2.svg"
                     alt=""
                 />
             </div>
@@ -140,29 +162,75 @@ export const App = () => {
                     <h2 className="cases-title">Направления кейсов</h2>
                     <div className="cases-list">
                         <div className="cases-item">
-                            <img src="image/cases/1.svg" alt="" />
+                            <img src="image/case-1.png" alt="" />
                         </div>
                         <div className="cases-item">
-                            <img src="image/cases/1.svg" alt="" />
+                            <img src="image/case-2.png" alt="" />
                         </div>
                         <div className="cases-item">
-                            <img src="image/cases/1.svg" alt="" />
+                            <img src="image/case-3.png" alt="" />
                         </div>
                         <div className="cases-item">
-                            <img src="image/cases/1.svg" alt="" />
+                            <img src="image/case-4.png" alt="" />
                         </div>
                         <div className="cases-item">
-                            <img src="image/cases/1.svg" alt="" />
+                            <img src="image/case-5.png" alt="" />
                         </div>
                         <div className="cases-item">
-                            <img src="image/cases/1.svg" alt="" />
+                            <img src="image/case-6.png" alt="" />
                         </div>
+                    </div>
+                </div>
+
+                <div class="steps-content max-app-width">
+                    <h2 class="steps-title">Этапы Проведения</h2>
+                    <div class="steps-list">
+                        <div class="step-item">
+                            <p class="step-number">1 <span>ЗАОЧНЫЙ</span></p>
+                            <div class="step-info-1">
+                                <div>
+                                    <p class="step-dates">21 сентября - 28 сентября</p>
+                                    <p class="step-description">Отборочный этап</p>
+                                </div>
+                                <div>
+                                    <p class="step-dates">29 сентября</p>
+                                    <p class="step-description">Объявление результатов отборочного этапа</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="step-item">
+                            <p class="step-number">2 <span>г. МОСКВА</span></p>
+                            <div class="step-info">
+                                <p class="step-dates">14 октября - 16 октября</p>
+                                <p class="step-description">Образовательная программа</p>
+                            </div>
+                        </div>
+                        <div class="step-item">
+                            <p class="step-number">3 <span>г. ТЮМЕНЬ</span></p>
+                            <div class="step-info">
+                                <p class="step-dates">29 ноября - 5 декабря</p>
+                                <p class="step-description">Заключительный этап</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="reg-block">
+                    <div className="reg-block-top">
+                        <a className="reg-block-top-btn" href="https://www.tyuiu.ru/" target="_blank">Положение</a>
+                        <a className="reg-block-top-btn" href="https://www.tyuiu.ru/" target="_blank">Регламент</a>
+                    </div>
+                    <a className="reg-block-register" href="https://t.me/kaskad_it_channel" target="_blank">Регистрация</a>
+                    <a className="reg-block-telegram" href="https://t.me/kaskad_it_channel" target="_blank">Telegram-Канал</a>
+                    <div className="reg-block-bot">
+                        <p>По всем вопросам:</p>
+                        <a href="mailto:info@касиннтех.рф">info@касиннтех.рф</a>
                     </div>
                 </div>
             </main>
 
             <footer className="footer">
-
+                <p>© 2024 Каскад Инновационных Технологий. Все права защищены.</p>
             </footer>
         </>
     )
